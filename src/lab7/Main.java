2d005f8c037935676a5a503086a5c968fdd92161
Emil Sroka <srokaemil@gmail.com>
2020-01-09 22:29:27 +0100

    Add simple main

---
+++ src/lab7/Main.java
@@ -0,0 +1,23 @@
+package lab7;
+
+public class Main {
+    public static void main(String[] args){
+        AdminUnitList list = new AdminUnitList();
+        list.read("admin-units.csv");
+        list.fixMissingValues();
+
+        AdminUnitQuery query = new AdminUnitQuery()
+                .selectFrom(list)
+                .where(a->a.area>1000)
+                .or(a->a.name.startsWith("Sz"))
+                .sort((a,b)->Double.compare(a.area,b.area))
+                .limit(100);
+        query.execute().list(System.out);
+
+        //list.filter(a->a.name.startsWith("Ż")).sortInPlaceByArea().list(System.out);
+
+        //list.filter(a->a.name.startsWith("K")).sortInPlaceByName().list(System.out);
+
+        //list.filter(a->a.adminLevel == 6 && a.parent.name.equals("województwo małopolskie")).sortInPlaceByName().list(System.out);
+    }
+}
